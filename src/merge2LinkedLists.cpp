30e8d53bdd6e1567053338dfa79726cc29c71778
sivakotapati <sivakotapati@missionrnd.com>
2016-03-31 07:28:40 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -28,30 +28,30 @@ struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
 		return head1;
 
 	//m is resultant list after merging two sorted linked lists i.e, head1,head2
-	struct node * m = NULL;
+	struct node * result = NULL;
 
 	//applying merge algorithm
 	for (; head1 != NULL&&head2 != NULL;){
 		if (head1->num <= head2->num){
-			add_node_at_end(&m, head1->num);
+			add_node_at_end(&result, head1->num);
 			head1 = head1->next;
 		}
 		else{
-			add_node_at_end(&m, head2->num);
+			add_node_at_end(&result, head2->num);
 			head2 = head2->next;
 		}
 	}
 	while (head1 != NULL){
-		add_node_at_end(&m, head1->num);
+		add_node_at_end(&result, head1->num);
 		head1 = head1->next;
 	}
 
 	while (head2 != NULL){
-		add_node_at_end(&m, head2->num);
+		add_node_at_end(&result, head2->num);
 		head2 = head2->next;
 	}
 
-	return m;
+	return result;
 }
 void add_node_at_end(struct node **h, int k){
 	//inserting node at end
